0ed56c67288ddec81f388cb4002082050a6f6036
Ayaka7452 <ayaka7452@users.noreply.github.com>
2026-09-20 14:45:58 +0800

    v1.12.2: 文件夹选择器的根目录选项去掉括号说明

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ayaka7452.daymate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 99
-        versionName = "1.12.1"
+        versionCode = 100
+        versionName = "1.12.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
