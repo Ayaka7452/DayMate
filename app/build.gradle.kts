bdb399936c72ee831720530b2b5bca55979d5865
Ayaka7452 <ayaka7452@users.noreply.github.com>
2026-09-23 17:07:43 +0800

    v1.13.2: 数据维护文案优化；文件夹推荐 emoji 移除音乐符号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ayaka7452.daymate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 110
-        versionName = "1.13.1"
+        versionCode = 111
+        versionName = "1.13.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
