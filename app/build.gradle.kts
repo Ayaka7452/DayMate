f3c45cd1e5439ea0f4459fef571b1ecd25d32db8
Ayaka7452 <ayaka7452@users.noreply.github.com>
2026-09-26 16:35:45 +0800

    版本号 1.18.0 (vc133)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ayaka7452.daymate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 132
-        versionName = "1.17.9"
+        versionCode = 133
+        versionName = "1.18.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
