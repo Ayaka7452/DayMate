061df565dd834c5cf6e29771242974a1a0660144
Ayaka7452 <ayaka7452@users.noreply.github.com>
2026-09-26 16:04:12 +0800

    版本号 1.17.8 (vc131)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ayaka7452.daymate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 130
-        versionName = "1.17.7"
+        versionCode = 131
+        versionName = "1.17.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
